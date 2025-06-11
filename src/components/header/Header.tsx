568d7df631020b09fef745151d654947e8f2a076
Tatyana Koba <misskotanyaz@gmail.com>
2025-06-11 16:55:46 +0300

    style: changed styles

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,26 +1,24 @@
-import { Bell, MapPin, Search, UserRound } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
-import { UserPanel } from "./UserPanel";
+import { MapPin } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { UserPanel } from './UserPanel';
 
 export function Header() {
-  return (
-    <header className="mx-auto my-9 px-4 w-11/12 flex justify-between items-center rounded-2xl bg-transparent shadow-md shadow-[#445478]">
-      <div className="flex items-center gap-1">
-        <MapPin size={16} />
-        <p>City</p>
-      </div>
+	return (
+		<header
+			className="absolute top-10 left-[50%] -translate-x-[50%] px-4 w-11/12 flex justify-between items-center z-50
+    rounded-2xl bg-transparent shadow-md shadow-[#2b2e4f]"
+		>
+			<div className="flex items-center gap-1">
+				<MapPin size={16} />
+				<p>City</p>
+			</div>
 
-      <Link href={'/'}>
-        <Image
-          src="../logo.svg"
-          alt="logo"
-          width={48}
-          height={48}
-        />
-      </Link>
+			<Link href={'/'}>
+				<Image src="../logo.svg" alt="logo" width={48} height={48} />
+			</Link>
 
-      <UserPanel />
-    </header>
-  )
+			<UserPanel />
+		</header>
+	);
 }
